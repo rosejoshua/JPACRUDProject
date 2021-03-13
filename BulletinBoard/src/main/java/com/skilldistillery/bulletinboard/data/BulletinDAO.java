0fdcf8e8a32b351c35efa5d5ac652ece36345b56
Joshua Rose <jlr007@protonmail.com>
2021-03-12 20:33:35 -0500

    completed showAllByAuthor() in BulletinDAOJpaImpl

--- BulletinBoard/src/main/java/com/skilldistillery/bulletinboard/data/BulletinDAO.java
+++ BulletinBoard/src/main/java/com/skilldistillery/bulletinboard/data/BulletinDAO.java
@@ -1,10 +1,16 @@
 package com.skilldistillery.bulletinboard.data;
 
+import java.util.List;
+
 import com.skilldistillery.bulletinboard.entities.Bulletin;
 
 public interface BulletinDAO {
 	
 	Bulletin findById(int bulletinId);
+	
+	List<Bulletin> showAllByAuthor(String author);
+	List<Bulletin> showAll(String author);
+	
 	
 
 }
--- BulletinBoard/src/main/java/com/skilldistillery/bulletinboard/data/BulletinDAOJpaImpl.java
+++ BulletinBoard/src/main/java/com/skilldistillery/bulletinboard/data/BulletinDAOJpaImpl.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.bulletinboard.data;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -17,8 +19,19 @@ public class BulletinDAOJpaImpl implements BulletinDAO {
 
 	@Override
 	public Bulletin findById(int bulletinId) {
-		// TODO Auto-generated method stub
 		return em.find(Bulletin.class, bulletinId);
+	}
+
+	@Override
+	public List<Bulletin> showAllByAuthor(String author) {
+		String jpql = "SELECT b FROM Bulletin b WHERE b.author = '" + author + "'";
+		return em.createQuery(jpql, Bulletin.class).getResultList();
+	}
+
+	@Override
+	public List<Bulletin> showAll(String author) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
