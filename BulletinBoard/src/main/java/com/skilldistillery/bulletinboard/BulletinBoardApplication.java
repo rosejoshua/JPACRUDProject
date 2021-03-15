b1b515ad72791f49de6fbab69d9ba7ac88ee16f5
Joshua Rose <jlr007@protonmail.com>
2021-03-15 11:16:19 -0400

    prepped Spring app for AWS

--- BulletinBoard/src/main/java/com/skilldistillery/bulletinboard/BulletinBoardApplication.java
+++ BulletinBoard/src/main/java/com/skilldistillery/bulletinboard/BulletinBoardApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.bulletinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BulletinBoardApplication {
+public class BulletinBoardApplication extends SpringBootServletInitializer {
+	
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(BulletinBoardApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BulletinBoardApplication.class, args);
